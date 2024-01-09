b352cd4360b707f1c1dee8b7cba9f33977164d5b
Guilherme230308 <guilherme_230308@hotmail.com>
2024-01-09 20:17:27 -0300

    Banner

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,9 @@
+function Banner() {
+    return (
+        <header>
+            <img src="/imagens/banner.png" alt="Banner principal da aplicação"/>
+        </header>
+    )
+}
+
+export default Banner;
