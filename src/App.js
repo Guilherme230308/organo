b98abe221b55ba15cd40aa79383a2bad6533bfc9
Guilherme230308 <guilherme_230308@hotmail.com>
2024-01-09 19:48:26 -0300

    Config

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Configuração do Git
+          Configuração do Github
         </p>
         <a
           className="App-link"
